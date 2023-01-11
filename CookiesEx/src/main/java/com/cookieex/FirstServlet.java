07fc19a62da3c64eb0c989e9e73e3253f8d10978
satya <satya7512@gmail.com>
2023-01-11 13:07:41 +0530

    Commiting Servlet1 and Servlet2

---
+++ CookiesEx/src/main/java/com/cookieex/FirstServlet.java
@@ -0,0 +1,47 @@
+package com.cookieex;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class FirstServlet
+ */
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FirstServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		String name = request.getParameter("name");
+		out.println("Welcome "+name);
+		
+		Cookie ck = new Cookie("name", name);
+		response.addCookie(ck);
+		
+		out.println("<form action='./servlet2' method='post'>");
+		out.println("<input type='submit' value='Go'>");
+		out.println("</form>");
+		
+		out.close();
+	}
+
+}
